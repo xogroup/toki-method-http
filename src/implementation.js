ce5aaf9dd9f0dec691dfca5a412526fc5c55d3dd
Derrick Hinkle <dhinkle@xogrp.com>
2017-03-15 16:25:06 -0500

    Implementation and initial tests

--- src/implementation.js
+++ src/implementation.js
@@ -1,18 +1,56 @@
 'use strict';
 
-/* Example singleton with es6
- * let myInstance = null;
- * const SomeClass = require('SomeClass');
- *
- * module.exports = function() {
- *     if (!myInstance) {
- *         myInstance = new SomeClass();
- *     }
- *
- *     return myInstance;
- * }
- *
-**/
+const Templater = require('toki-templater');
+const Request = require('superagent');
+const Joi = require('joi');
+const Promise = require('bluebird');
 
-//export result here
-module.exports = {};
+const configSchema = Joi.object().keys({
+    url: Joi.string().uri().required(),
+    passThroughHeaders: Joi.alternatives().try(Joi.boolean(), Joi.array()),
+    headers: Joi.object(),
+    payload: Joi.any(),
+    method: Joi.string().allow(['get', 'post', 'put', 'del', 'patch']).default('get')
+});
+
+module.exports = function () {
+
+    return Promise
+    .resolve()
+    .bind(this)
+    .then( () => Templater(this.action, configSchema) )
+    .then((config) => {
+
+        this.config = config;
+    })
+    .then(() => {
+        //url
+        const req = Request(this.config.method, this.config.url);
+
+        //body
+        if (this.config.payload) {
+            req.send(this.config.payload);
+        }
+
+        //proxy along  headers
+        if (this.config.passThroughHeaders) {
+            if (this.config.passThroughHeaders === true) {
+                req.set(this.request.headers);
+            }
+            else {
+                this.config.passThroughHeaders.forEach( (header) => {
+
+                    req.set(header, this.request.headers[header]);
+                });
+            }
+        }
+
+        //fixed headers which override pass through
+        if (this.config.headers) {
+            req.set(this.config.headers);
+        }
+
+        return req;
+    });
+
+};
--- test/implementation.js
+++ test/implementation.js
@@ -1,18 +1,142 @@
 'use strict';
 
 const expect = require('code').expect;   // assertion library
+const Promise = require('bluebird');
 const Lab = require('lab');
+const Hapi = require('hapi');
+const Sinon = require('sinon');
 const lab = exports.lab = Lab.script();
-const describe = lab.describe;
-const it = lab.it;
+const { describe, it, before, beforeEach } = lab;
 
-const Foo = require('../src/implementation');
+const HttpMethod = require('../src/implementation');
+let server;
+const actionSpies = {};
 
-describe('Foo', () => {
+describe('HTTP Method', () => {
 
-    it('checks to make sure Foo is an object', (done) => {
+    before((done) => {
 
-        expect(Foo).to.be.a.object();
-        done();
+        server = new Hapi.Server();
+        server.connection({
+            port: 5000
+        });
+
+        actionSpies.get = Sinon.spy();
+        actionSpies.post = Sinon.spy();
+
+        server.route({
+            method: 'GET',
+            path: '/test',
+            handler: (req, reply) => {
+
+                reply(200);
+                return actionSpies.get(req.body, req.headers);
+            }
+        });
+
+        server.route({
+            method: 'POST',
+            path: '/test',
+            handler: (req, reply) => {
+
+                reply(201);
+                return actionSpies.post(req.body, req.headers);
+            }
+        });
+
+        return server.start(done);
+    });
+
+    beforeEach( (done) => {
+
+        actionSpies.get.reset();
+        actionSpies.post.reset();
+
+        return done();
+    });
+
+    it('should throw an error if no url is provided', { plan: 1 }, () => {
+
+        return Promise
+        .resolve()
+        .bind({
+            action: {}
+        })
+        .then(HttpMethod)
+        .catch( (e) => {
+
+            expect(e).to.be.instanceof(Error);
+        });
+    });
+
+    it('makes a get request', () => {
+
+        return Promise
+        .resolve()
+        .bind({
+            action: {
+                url: 'http://localhost:5000/test'
+            }
+        })
+        .then(HttpMethod)
+        .then( () => {
+
+            expect(actionSpies.get.calledOnce).to.be.true();
+        });
+    });
+
+    it('makes a post request', () => {
+
+        return Promise
+        .resolve()
+        .bind({
+            action: {
+                url: 'http://localhost:5000/test',
+                method: 'post'
+            }
+        })
+        .then(HttpMethod)
+        .then( () => {
+
+            expect(actionSpies.post.calledOnce).to.be.true();
+        });
+    });
+
+    it('makes a post request', () => {
+
+        return Promise
+        .resolve()
+        .bind({
+            action: {
+                url: 'http://localhost:5000/test',
+                method: 'post'
+            }
+        })
+        .then(HttpMethod)
+        .then( () => {
+
+            expect(actionSpies.post.calledOnce).to.be.true();
+        });
+    });
+
+    it('makes a post request', () => {
+
+        const testHeader = { 'x-arbitrary': 'Foobar' };
+        return Promise
+        .resolve()
+        .bind({
+            action: {
+                url: 'http://localhost:5000/test',
+                method: 'post',
+                headers: testHeader
+            }
+        })
+        .then(HttpMethod)
+        .then( () => {
+
+            expect(actionSpies.post.calledOnce).to.be.true();
+            const spyArgs = actionSpies.post.getCall(0).args;
+            expect(spyArgs[1]).to.include(testHeader);
+        });
     });
 });
